b9e9c64c4eb606fd8c205663c31fce16bfde68e2
runyu1413 <79622204+runyu1413@users.noreply.github.com>
2023-11-10 03:24:17 +0800

    Implemented QEP in Natural Language

--- explore.py
+++ explore.py
@@ -1,80 +1,217 @@
-import psycopg2
 import json
-import os
-from dotenv import load_dotenv
+import psycopg2
 
-load_dotenv()
+operatorSeqList = []
+parents = []
+informations = []
+timing = []
+listofAnnotations = []
+executionTime = 0
+planningTime = 0
+totalCost = 0
 
-def connect_to_db():
+def startAnnotation():
+    clearAnnotation()
+    queryplan = "queryplan.json"
+    plan = getPlan(queryplan)
+    getFullPlan(plan)
+    annotate()
+    return listofAnnotations
+
+def clearAnnotation():
+    operatorSeqList.clear()
+    parents.clear()
+    informations.clear()
+    timing.clear()
+    listofAnnotations.clear()
+
+def getPlan(filename):
+    with open(f"{filename}") as file:
+        data = json.load(file)
+        plan = data[0][0][0]["Plan"]
+        global executionTime
+        global planningTime
+        global totalCost
+        executionTime = data[0][0][0]["Execution Time"]
+        planningTime = data[0][0][0]["Planning Time"]
+        totalCost = data[0][0][0]["Plan"]["Total Cost"]
+    return plan
+
+def getFullPlan(plan):
+    planIterator(plan, -1)
+    for i in informations:
+        if 'Plans' in i:
+            i.pop('Plans')
+
+def planIterator(queryplan, parentNo):
+    lastNum = parentNo
+    operatorSeqList.append(queryplan['Node Type'])
+    parents.append(parentNo)
+    informations.append(queryplan)
+    timing.append(queryplan["Actual Total Time"])
+    lastNum = len(parents) - 1
+    if "Plans" in queryplan:
+        for plan in queryplan["Plans"]:
+            planIterator(plan, lastNum)
+    else:
+        return
+        
+def annotate():
+    intermediateTables = []
+    intermediateTableCount = 1
+    count = 1
+    descriptions = ''
+    i = len(operatorSeqList) - 1
+    while i >= 0:
+        nodeType = operatorSeqList[i]
+        # If performing Sequential Scan
+        if nodeType == 'Seq Scan':
+            descriptions += f'Step {count}: Sequential Scan is performed on \'{informations[i]["Relation Name"]}\''
+            if 'Filter' in informations[i]:
+                descriptions+= f' due to low selectivity of {informations[i]["Filter"]}'
+            descriptions += '.\n'
+            intermediateTables.append(informations[i]["Relation Name"])
+        # If performing Index Scan
+        elif nodeType == 'Index Scan':
+            descriptions += f'Step {count}: Index Scan is performed on \'{informations[i]["Relation Name"]}\' due to high selectivity of'
+            if 'Filter' in informations[i]:
+                descriptions+= f' {informations[i]["Filter"]}'
+            intermediateTables.append(informations[i]["Relation Name"])
+            # If index cond exist
+            if "Index Cond" in informations[i]:
+                descriptions += f' {informations[i]["Index Cond"]}'
+            descriptions += '.\n'
+        # If performing Index Only Scan
+        elif nodeType == 'Index Only Scan':
+            descriptions += f'Step {count}: Index Only scan is performed on \'{informations[i]["Index Name"]}\'as data could be access from indexes directly'
+            intermediateTables.append(informations[i]["Relation Name"])
+            # If index cond exist
+            if "Index Cond" in informations[i]:
+                descriptions += f' under condition(s): {informations[i]["Index Cond"]}'
+            if 'Filter' in informations[i]:
+                descriptions+= f' with filter{informations[i]["Filter"]}'
+            descriptions += '.\n'
+        # If performing Bitmap Heap Scan operation
+        elif nodeType == 'Bitmap Heap Scan':
+            descriptions += f'Step {count}: Bitmap Heap Scan is performed on \'{informations[i]["Relation Name"]}\' to decode bitmap'
+            if 'Filter' in informations[i]:
+                descriptions+= f' with filter {informations[i]["Filter"]}'
+            intermediateTables.append(informations[i]["Relation Name"])
+            descriptions += '.\n'
+        # If performing Hash operation
+        elif nodeType == 'Hash':
+            descriptions += f'Step {count}: Hashing is performed on \'{intermediateTables[-1]}\'.\n'
+        # If performing Hash Join operation
+        elif nodeType == 'Hash Join':
+            descriptions += f'Step {count}: Hash {informations[i]["Join Type"]} join is performed under condition(s) of {informations[i]["Hash Cond"]} for {intermediateTables.pop(0)} and {intermediateTables.pop(0)} to generate intermediate table T{intermediateTableCount} as it is ideal to process inputs that are large, unsorted and non-indexed.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Merge Join operation
+        elif nodeType == 'Merge Join':
+            descriptions += f'Step {count}: Merge Join is performed for {intermediateTables.pop(0)} and {intermediateTables.pop(0)} under condition(s) of {informations[i]["Merge Cond"]} to generate intermediate table T{intermediateTableCount}. This is done because join inputs are large and are sorted on their join attributes.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Merge Append operation
+        elif nodeType == 'Merge Append':
+            descriptions += f'Step {count}: Results from table {intermediateTables.pop(0)} are appended to {intermediateTables.pop(0)} to generate intermediate table T{intermediateTableCount}.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Aggregation operation
+        elif nodeType == 'Aggregate':
+            if informations[i]['Strategy'] == "Hashed":
+                descriptions += f'Step {count}: Aggregate is performed to hash rows of {intermediateTables[-1]} based on {informations[i]["Group Key"]}, and resulting rows will be returned.\n'
+            if informations[i]['Strategy'] == "Sorted":
+                descriptions += f'Step {count}: Aggregate is performed to sort rows of {intermediateTables[-1]} based on {informations[i]["Group Key"]}, and resulting rows will be returned.\n'
+            if informations[i]['Strategy'] == "Plain":
+                descriptions += f'Step {count}: Aggregate is performed on {intermediateTables[-1]} and resulting rows will be returned.\n'
+        # If performing Gather operation
+        elif nodeType == 'Gather':
+            descriptions += f'Step {count}: Gather is performed on {intermediateTables[-1]} to get intermediate table T{intermediateTableCount}.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Gather Merge operation
+        elif nodeType == 'Gather Merge':
+            descriptions += f'Step {count}: Gather Merge is performed on {intermediateTables[-1]}.\n'
+        # If performing Append operation
+        elif nodeType == 'Append':
+            descriptions += f'Step {count}: Append results from {intermediateTables.pop(0)} to {intermediateTables.pop(0)} to get intermediate table T{intermediateTableCount}.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Sort operation
+        elif nodeType == 'Sort':
+            descriptions += f'Step {count}: Sort is performed on rows in {intermediateTables[-1]} based on {informations[i]["Sort Key"]})\n'
+            if informations[i]["Sort Key"][0].find("INC") != -1:
+                descriptions += f'in ascending order.\n'
+            if informations[i]["Sort Key"][0].find("DESC") != -1:
+                descriptions += f'in descending order.\n'
+        # If performing Incremental Sort operation
+        elif nodeType == 'Incremental Sort':
+            descriptions += f'Step {count}: Incremental Sort is performed on {intermediateTables[-1]} with Sort Key {informations[i]["Sort Key"]} \n'
+        # If performing Materialize operation
+        elif nodeType == 'Materialize':
+            descriptions += f'Step {count}: Materialization is performed on {intermediateTables[-1]}.\n'
+        # If performing Modify Table operation
+        elif nodeType == 'ModifyTable':
+            descriptions += f'Step {count}: Modification is performed on \'{informations[i]["Relation Name"]}\'.\n'
+        # If performing CTE_Scan operation
+        elif nodeType == 'CTE_Scan':
+            descriptions += f'Step {count}: CTE_Scan is performed on relation'
+            intermediateTables.append(informations[i]["Relation Name"])
+            # If index cond exist
+            if "Index Cond" in informations[i]:
+                descriptions += f' with condition(s): {informations[i]["Index Cond"]}.\n'
+        # If performing Nested Loop operation
+        elif nodeType == 'Nested Loop':
+            descriptions += f'Step {count}: Nested Loop operation is performed for {intermediateTables.pop(0)} and {intermediateTables.pop(0)} to generate intermediate table T{intermediateTableCount} as one join input is small  (e.g., fewer than 10 rows) and other join input is large and indexed on its join attributes.\n'
+            intermediateTables.append(f'T{intermediateTableCount}')
+            intermediateTableCount += 1
+        # If performing Limit operation
+        elif nodeType == 'Limit':
+            descriptions += f'Step {count}: Perform Limit to limit scanning to {informations[i]["Plan Rows"]} rows.\n'
+        elif nodeType == 'SetOp':
+            descriptions += f'Step {count}: Set operation is performed on {intermediateTables[-1]}\n'
+        elif nodeType == 'Memoize':
+            descriptions += f'Step {count}: Perform Memoize of {intermediateTables[-1]} with respect to Cache key: {informations[i]["Cache Key"]}.\n'
+        else:
+            descriptions += f'Undefined node found!.\n'
+        count += 1
+        i -= 1
+    descriptions += f"\nExecution Time = {executionTime} ms\n"
+    descriptions += f"Planning Time = {planningTime}ms\n"
+    descriptions += f"Total Cost = {totalCost}\n"
+    listofAnnotations.append(descriptions)
+    return descriptions
+
+def runQuery(text, port_value, host_value, database_value, user_value, password_value):
+    sql_string = "EXPLAIN (analyze, verbose, costs, format JSON) " + text
+
     try:
-        conn = psycopg2.connect(
-            dbname=os.getenv("POSTGRES_DBNAME"),  # Replace with your database name
-            user=os.getenv("POSTGRES_USERNAME"),  # Replace with your username
-            password=os.getenv("POSTGRES_PASSWORD"),
-            host=os.getenv("POSTGRES_HOST"),
-            port=os.getenv("POSTGRES_PORT"),
+        con = psycopg2.connect(
+            port=port_value,
+            host=host_value,
+            database=database_value,
+            user=user_value,
+            password=password_value
         )
-        return conn
-    except Exception as e:
-        print(f"Error connecting to the database: {e}")
-        return None
-
-def run_explain_query(conn, query):
-    try:
-        cursor = conn.cursor()
-        # Execute an EXPLAIN (FORMAT JSON) query
-        cursor.execute(f"EXPLAIN (BUFFERS ON, ANALYZE ON, COSTS ON, VERBOSE ON, SUMMARY ON, TIMING ON, FORMAT JSON) {query}")
-        # Fetch the result
-        result = cursor.fetchone()
-        # Convert the result to JSON
-        explain_json = json.loads(json.dumps(result[0]))
-        return explain_json
-
-    except Exception as e:
-        print(f"Error executing the query: {e}")
-        return None
-    finally:
+        cursor = con.cursor()
+        cursor.execute(sql_string)
+        queryOutput = cursor.fetchall()
+        queryExecuted = True
         cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        queryOutput = "Please check your sql statement: \n" + text
+        queryExecuted = False
 
-def print_plan(plan, level=0):
-    indent = "    " * level
-    node_type = plan.get("Node Type", "Unknown")
-    relation = plan.get("Relation Name", "N/A")
-    alias = plan.get("Alias", "")
-    relation_info = f" on {relation}" if relation != "N/A" else ""
-    alias_info = f" (alias: {alias})" if alias else ""
-    print(f"{indent}- {node_type}{relation_info}{alias_info}")
-    if "Plans" in plan:
-        for subplan in plan["Plans"]:
-            print_plan(subplan, level + 1)
+    with open('queryplan.json', 'w') as f:
+        json.dump(queryOutput, f, ensure_ascii=False, indent=2)
 
-def main():
-    conn = connect_to_db()
-    if conn:
-        # Sample query from TPC-H (modify as needed)
-        # sample_query = "SELECT l_returnflag, l_linestatus, SUM(l_quantity) FROM lineitem GROUP BY l_returnflag, l_linestatus"
-        sample_query = """SELECT
-    o_orderkey,
-    o_orderdate,
-    o_totalprice,
-    c_name AS customer_name
-FROM
-    orders o
-JOIN
-    customer c ON o.o_custkey = c.c_custkey
-WHERE
-    o.o_orderstatus = 'O'
-ORDER BY
-    o.o_totalprice DESC
-LIMIT 10;"""
+    return queryExecuted
 
-        explain_json = run_explain_query(conn, sample_query)
-        if explain_json:
-            print("EXPLAIN Plan Tree:")
-            for plan in explain_json:
-                print_plan(plan["Plan"])
-        # Close the connection
-        conn.close()
+# function to create the QEP Tree Diagram TO-DO
+def createQEPTreeDiagram():
 
-if __name__ == "__main__":
-    main()
+    with open('queryplan.json') as json_file:
+        data = json.load(json_file)
+    dict_plan_inner = data[0][0]
+
+    #TO-DO
--- project.py
+++ project.py
@@ -1,164 +1,169 @@
 import tkinter as tk
+import customtkinter as ctk
+from explore import *
 import psycopg2
+import json
 
-class HoverButton(tk.Button):
-    def __init__(self, master, **kw):
-        tk.Button.__init__(self, master=master, **kw)
-        self.defaultBackground = self["background"]
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
+def login():
+    global port
+    global host
+    global database
+    global user
+    global password
+    
+    port = port_entry.get()
+    host = host_entry.get()
+    database = database_entry.get()
+    user = user_entry.get()
+    password = password_entry.get()
 
-    def on_enter(self, e):
-        self['background'] = self['activebackground']
+    try:
+        conn = psycopg2.connect(
+            port=port,
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+        conn.close()
+        login_window.destroy()
+        create_query_window()
 
-    def on_leave(self, e):
-        self['background'] = self.defaultBackground
+    except psycopg2.Error as e:
+        error_label.config(text=f"Error: {e}", fg="red")
 
 def create_login_window():
-    def login():
-        # Retrieve database connection information from the input fields
-        host = host_entry.get()
-        database = database_entry.get()
-        user = user_entry.get()
-        password = password_entry.get()
-
-        try:
-            # Connect to the PostgreSQL database
-            conn = psycopg2.connect(
-                host=host,
-                database=database,
-                user=user,
-                password=password
-            )
-            conn.close()  # Close the connection
-
-            # Close the login window and open the query window
-            login_window.destroy()
-            create_query_window(host, database, user, password)
-
-        except psycopg2.Error as e:
-            # Display an error message if there's a connection issue
-            error_label.config(text=f"Error: {e}", fg="red")
+    global login_window
+    global port_entry
+    global host_entry
+    global database_entry
+    global user_entry
+    global password_entry
+    global error_label
 
     login_window = tk.Tk()
     login_window.title("Login to PostgreSQL")
     login_window.geometry('330x480')
     login_window.config(bg='white')
     
-    frame = tk.Frame(login_window, bg='white')
+    frame = ctk.CTkFrame(login_window, fg_color='white')
 
-    title_label = tk.Label(frame, text="PostgreSQL", font=('Arial', 30))
+    title_label = ctk.CTkLabel(frame, text="PostgreSQL", font=('Arial', 30, 'bold'), text_color='black')
 
-    host_label = tk.Label(frame, text="Host", bg='white', font=('Arial', 14))
-    host_entry = tk.Entry(frame, bg = '#D3D3D3', relief='solid', font=('Arial', 14))
+    port_label = ctk.CTkLabel(frame, text="Port", font=('Arial', 14), text_color='black')
+    port_entry = ctk.CTkEntry(frame, fg_color = '#D3D3D3', font=('Arial', 14), text_color='black')
+    
+    host_label = ctk.CTkLabel(frame, text="Host", font=('Arial', 14), text_color='black')
+    host_entry = ctk.CTkEntry(frame, fg_color = '#D3D3D3', font=('Arial', 14), text_color='black')
 
-    database_label = tk.Label(frame, text="Database", bg='white', font=('Arial', 14))
-    database_entry = tk.Entry(frame, bg = '#D3D3D3', relief='solid', font=('Arial', 14))
+    database_label = ctk.CTkLabel(frame, text="Database", font=('Arial', 14), text_color='black')
+    database_entry = ctk.CTkEntry(frame, fg_color = '#D3D3D3', font=('Arial', 14), text_color='black')
 
-    user_label = tk.Label(frame, text="Username", bg='white', font=('Arial', 14))
-    user_entry = tk.Entry(frame, bg = '#D3D3D3', relief='solid', font=('Arial', 14))
+    user_label = ctk.CTkLabel(frame, text="Username", font=('Arial', 14), text_color='black')
+    user_entry = ctk.CTkEntry(frame, fg_color = '#D3D3D3', font=('Arial', 14), text_color='black')
 
-    password_label = tk.Label(frame, text="Password", bg='white', font=('Arial', 14))
-    password_entry = tk.Entry(frame, show="*",bg = '#D3D3D3', relief='solid', font=('Arial', 14))
+    password_label = ctk.CTkLabel(frame, text="Password", font=('Arial', 14), text_color='black')
+    password_entry = ctk.CTkEntry(frame, show="*",fg_color = '#D3D3D3', font=('Arial', 14), text_color='black')
 
-    login_button = HoverButton(frame, text="Login", width='25', font=('Arial', 14), fg="white", bg='#24a0ed',
-                activebackground='#237fb7', command=login)
-
-    error_label = tk.Label(frame, text="", fg="red")
+    login_button = ctk.CTkButton(frame, text="Login", width=200, font=('Arial', 14), text_color="white", fg_color='#24a0ed', hover_color='#237fb7', command=login)
+    
+    error_label = ctk.CTkLabel(frame, text="", text_color="red")
 
     title_label.grid(row=0, column=0, columnspan=2, sticky="nsew", pady=30)
-    host_label.grid(row=1, column=0, sticky="w")
-    host_entry.grid(row=1, column=1, pady=10)
-    database_label.grid(row=2, column=0, sticky="w")
-    database_entry.grid(row=2, column=1, pady=10)
-    user_label.grid(row=3, column=0, sticky="w")
-    user_entry.grid(row=3, column=1, pady=10)
-    password_label.grid(row=4, column=0, sticky="w")
-    password_entry.grid(row=4, column=1, pady=10)
-    login_button.grid(row=5, columnspan=2, pady=20)
-    error_label.grid(row=6, columnspan=2)
+    port_label.grid(row=1, column=0, sticky="w")
+    port_entry.grid(row=1, column=1, padx= 10, pady=10)   
+    host_label.grid(row=2, column=0, sticky="w")
+    host_entry.grid(row=2, column=1, padx= 10, pady=10)
+    database_label.grid(row=3, column=0, sticky="w")
+    database_entry.grid(row=3, column=1, padx= 10, pady=10)
+    user_label.grid(row=4, column=0, sticky="w")
+    user_entry.grid(row=4, column=1, padx= 10, pady=10)
+    password_label.grid(row=5, column=0, sticky="w")
+    password_entry.grid(row=5, column=1, padx= 10, pady=10)
+    login_button.grid(row=6, columnspan=2, pady=20)
+    error_label.grid(row=7, columnspan=2)
 
     frame.pack()
 
     login_window.mainloop()
-    
 
-def create_query_window(host, database, user, password):
-    def execute_query_and_display_plan():
-        # Retrieve the SQL query from the input field
-        query = user_query.get("1.0", tk.END)
+def submit_query():
+    query = user_query.get('1.0', 'end-1c')
+    qep_panel_text.configure(state='normal')
+    qep_panel_text.delete('1.0', 'end-1c')
 
-        try:
-            # Connect to the PostgreSQL database
-            conn = psycopg2.connect(
-                host=host,
-                database=database,
-                user=user,
-                password=password
-            )
+    if not query:
+        qep_panel_text.insert(tk.END, "Please submit SQL query\n")
+        qep_panel_text.config(fg='red')
+        qep_panel_text.configure(state='disabled')
 
-            # Create a cursor and execute the query
-            cursor = conn.cursor()
-            cursor.execute("EXPLAIN ANALYZE " + query)
-            query_plan = cursor.fetchall()
+    else:
+        x = runQuery(query, port, host, database, user, password)
+        if not x:
+            qep_panel_text.config(fg='red')
+            qep_panel_text.insert(tk.END, "Please check your SQL query")
+            qep_panel_text.configure(state='disabled')
+        else:
+            annotated_query = startAnnotation()
+            qep_panel_text.config(fg='black')
+            qep_panel_text.insert(tk.END,annotated_query[0])
+        qep_panel_text.configure(state='disabled')
 
-            # Display the query plan in the output text widget
-            query_plan_output.delete("1.0", tk.END)
-            for row in query_plan:
-                query_plan_output.insert(tk.END, f"{row[0]}\n")
+def clearQueries():
+    qep_panel_text.configure(state='normal')
+    qep_panel_text.delete('1.0', 'end-1c')
+    qep_panel_text.configure(state='disabled')
+    user_query.delete('1.0','end-1c')
+    clearAnnotation()
 
-            conn.close()  # Close the connection
+def create_query_window():
+    global window
+    global user_query
+    global qep_panel_text
+    global leftPanel_text
+    global centrePanel_text
+    global rightPanel_text
+    global error_label
 
-        except psycopg2.Error as e:
-            # Display an error message if there's a connection issue
-            query_plan_output.delete("1.0", tk.END)
-            query_plan_output.insert(tk.END, f"Error: {e}")
+    window = tk.Tk()
+    window.geometry("700x580")
+    window.title("Database")
+    window.config(bg = 'white')
 
-    def reset_query():
-        # Clear the SQL query input field
-        user_query.delete("1.0", tk.END)
-        query_plan_output.delete("1.0", tk.END)
-    
-    query_window = tk.Tk()
-    query_window.geometry('720x600')
-    query_window.title("PostgreSQL Query Plan")
-    query_window.config(bg='white')
-
-    # SQL Query Window display settings
     querypanel = tk.PanedWindow(bg='white')
-    querypanel_label = tk.Label(querypanel, text="Input SQL Query Here", bg='white', font=("Arial", 12, 'bold'))
+    querypanel_label = ctk.CTkLabel(querypanel, text="SQL Query", font=('Arial', 20, 'bold'), text_color='black')
     querypanel_label.pack(pady=5)
     querypanel.pack()
 
-    # Textbox for SQL Statement
     user_query = tk.Text(querypanel,height=9, relief='solid', wrap='word', bg = '#D3D3D3', font=('Arial',10))
     user_query.pack()
 
-    # Panel for button
     div = tk.PanedWindow(bg='white')
 
-    # Call preprocessing.py to execute query
-    submitButton = HoverButton(div, text="Submit Query", fg="white", bg = '#024d23', activebackground = '#04c256', relief=tk.RAISED, font=("Arial", 12), width=20, command=execute_query_and_display_plan)
+    submitButton = ctk.CTkButton(div, text='Submit', text_color = 'white', fg_color = '#04c256', hover_color = '#024d23', font=('Arial', 12), width = 200, command=submit_query)
     submitButton.pack(side=tk.LEFT, padx=5)
-
-    #Clear user query
-    clearbtn = HoverButton(div, text="Clear", relief=tk.RAISED, fg="white", bg = '#5c040a', activebackground = '#c20411',font=("Arial", 12), width=20,
-                     command=reset_query)
-    clearbtn.pack(side= tk.LEFT, padx=5)
-
+    
     div.pack(pady=5)
-    
-    # Query Plan Label display settings
-    query_plan = tk.PanedWindow(bg='white')
-    query_plan_label = tk.Label(query_plan, text="Query Execution Plan", bg='white', font=("Arial", 12, 'bold'))
-    query_plan_label.pack(pady=5)
-    query_plan.pack()
-    
-    # Query Plan Window display settings
-    query_plan_output = tk.Text(query_window, height=20, width=80, font=('Arial', 10), bg = '#D3D3D3', relief='solid', wrap='word')
-    query_plan_output.pack()
 
-    query_window.mainloop()
+    annotatedPanel = tk.PanedWindow(bg= 'white')
+    annotatedPanel_label = ctk.CTkLabel(annotatedPanel, text="Query Execution Plan In Natural Language", font=('Arial', 20, 'bold'), text_color='black')
+    annotatedPanel_label.pack(pady=5)
+    annotatedPanel.pack()
 
-# Start with the login window
-create_login_window()
+    qep_panel_text = tk.Text(annotatedPanel, state='disabled', height=14, relief='solid', wrap='word', font=('Arial', 10), bg = '#D3D3D3', width = 80)
+    qep_panel_text.pack()
+
+    div1 = tk.PanedWindow(bg='white')
+
+    qeptreebtn = ctk.CTkButton(div1, text="View QEP Tree", text_color = "white", fg_color = '#24a0ed', hover_color = '#237fb7', font=('Arial', 12), width = 200,command=createQEPTreeDiagram)
+    qeptreebtn.pack(side=tk.LEFT)
+
+    clearbtn = ctk.CTkButton(div1, text="Reset", text_color = "white", fg_color = '#c20411', hover_color = '#5c040a',font=('Arial', 12), width = 200, command=clearQueries)
+    clearbtn.pack(side= tk.LEFT, padx=5)
+    
+    div1.pack(pady=5)
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    create_login_window()
