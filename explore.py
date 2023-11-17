5339206855db494fdb8876c14eaaa67e0e07125d
AryanSethi20 <aryansethi20@gmail.com>
2023-11-17 21:26:18 +0800

    Structuring the code + final integration of backend and frontend

--- explore.py
+++ explore.py
@@ -18,102 +18,6 @@ app = Flask(__name__)
 
 SECRET_KEY = b'DBSP_PROJECT_2'
 
-def extract_string_before_word(text, word):
-    '''
-    Function used to extract the subquery from a list of subqueries
-    '''
-    # Find the position of the word
-    word_pos = text.find(word)
-    if word_pos == -1:
-        return None  # Word not found
-
-    # Iterate backwards to find the first closing bracket before the word
-    for i in range(word_pos, -1, -1):
-        if text[i] == ')':
-            closing_bracket_pos = i
-            break
-    else:
-        return None  # Closing bracket not found
-
-    # print(closing_bracket_pos)
-    # Use a stack to find the corresponding opening bracket
-    stack = []
-    for i in range(closing_bracket_pos, -1, -1):
-      if text[i]==')':
-        stack.append(')')
-      elif text[i]=='(':
-        stack.pop()
-        if len(stack)==0:
-          return text[i+1:closing_bracket_pos]
-    return None
-
-def extract_table_names(query):
-    '''
-    Function used to find out all the tables in the queries and its aliases
-    '''
-    parser = Parser(query)
-    query = parser.generalize
-    parser = Parser(query)
-        
-    relations = parser.tables
-    # print(relations)
-    pattern_for_subquery_alias = r'\(.SELECT.*?\)\s+AS\s+(\w+)'
-    # pattern_for_subquery_columns = r'\(.SELECT.)'
-    # pattern = r'\(([^()]*\([^()]*\))*[^()]*\)\s+AS\s+(\w+)'
-
-    # Find all matches in the query
-    matches = re.findall(pattern_for_subquery_alias, query, re.IGNORECASE | re.DOTALL)
-    print("Aliases: ", matches)
-    sql_keywords = [
-        'SELECT', 'FROM', 'WHERE', 'GROUP BY', 'HAVING', 'ORDER BY', 'JOIN',
-        'INNER JOIN', 'LEFT JOIN', 'RIGHT JOIN', 'OUTER JOIN', 'ON', 'AS',
-        'DISTINCT', 'UNION', 'INSERT', 'INTO', 'VALUES', 'UPDATE', 'SET',
-        'DELETE', 'CREATE', 'TABLE', 'ALTER', 'ADD', 'DROP', 'INDEX',
-        'CONSTRAINT', 'PRIMARY KEY', 'FOREIGN KEY', 'REFERENCES', 'CASCADE',
-        'TRUNCATE', 'COMMIT', 'ROLLBACK', 'BEGIN', 'END', 'IF', 'ELSE',
-        'CASE', 'WHEN', 'THEN', 'EXISTS'
-        ]
-    subqueries = []
-    # pdb.set_trace()
-    for match in matches:
-        subquery = extract_string_before_word(query,match)
-        if subquery!=None:
-            subqueries.append(subquery)
-    # for k, v in matches:
-    #     for i in range(len(relations)):
-    #         if relations[i]==v and k.upper() not in sql_keywords:
-    #             relations[i] = k
-    #             break
-    print(subqueries)
-    return relations
-
-def ctid_query(query):
-    '''Extract block and position in block using ctid'''
-    print('ctid_query')
-    relations=extract_table_names(query)
-    #ctid_list=[]
-    
-    modified_query_ctid="SELECT "
-    if 'group by' in query.lower(): #If there is GROUP BY clause
-        for i,relation in enumerate(relations):
-            modified_query_ctid+=f'ARRAY_AGG({relation}.ctid) AS {relation}_ctid '
-            #ctid_list.append(f'{relation}_ctid')
-            if i+1<len(relations):
-                modified_query_ctid+=', '
-    else:
-        for i,relation in enumerate(relations):
-                modified_query_ctid +=f"{relation}.ctid "
-                if i+1<len(relations):
-                    modified_query_ctid+=', '
-    
-    from_index = query.upper().find('FROM')
-    modified_query_ctid+=query[from_index:]
-    
-    order_index =modified_query_ctid.upper().find('ORDER BY')
-    modified_query_ctid=modified_query_ctid[:order_index]
-
-    return modified_query_ctid
-
 def remove_linebreaks_and_extra_spaces(query):
     '''
     Helper function used to remove all the linebreaks and whitespaces from the query
@@ -124,99 +28,6 @@ def remove_linebreaks_and_extra_spaces(query):
     query_without_extra_spaces = re.sub(r'\s+', ' ', query_without_linebreaks)
     # Strip leading and trailing spaces
     return query_without_extra_spaces.strip()
-
-def add_ctids_to_query(query, tables):
-    '''
-    This function takes a query as an input and returns a list of all the tables being accessed in that list
-    '''
-    query = query.upper()
-    parser = Parser(query)
-
-    if tables==None:
-        tables = parser.tables
-
-    print("Tables:", tables)
-    # Splitting the query into parts
-    parts = query.split("SELECT", 1)
-
-    # Handling edge case where "SELECT" isn't found or is used in column names, table names, etc.
-    if len(parts) < 2:
-        raise ValueError("Query does not contain a SELECT statement or is malformed.")
-
-    before_select, after_select = parts[0], "SELECT" + parts[1]
-
-    # Adding ctid for each table
-    ctid_selects = [f"{table}.ctid AS {table}_ctid" for table in tables]
-    ctid_group_by_selects = [f"{table}.ctid" for table in tables]
-    modified_select = after_select.replace("SELECT", "SELECT " + ", ".join(ctid_selects) + ", ", 1)
-    modified_select = modified_select.replace("GROUP BY", "GROUP BY "+ ", ".join(ctid_group_by_selects) + ", ", 1)
-
-    # Reassembling the query
-    modified_query = before_select + modified_select
-    order_index = modified_query.upper().find('ORDER BY')
-    modified_query=modified_query[:order_index]
-    
-    print(modified_query)
-    return modified_query
-
-def find_alias(sql_query):
-    '''
-    Function receives a subquery token (more than one), and returns all the aliases
-    '''
-    stack = []
-    aliases = []
-    i = 0
-
-    while i < len(sql_query):
-        char = sql_query[i]
-
-        if char == '(':
-            stack.append(i)
-        elif char == ')' and stack:
-            start = stack.pop()
-
-            if not stack:  # Stack is empty, indicating the end of a subquery
-                # Extract alias using regex
-                match = re.search(r'\)\s+AS\s+(\w+)', sql_query[i:])
-                if match:
-                    aliases.append(match.group(1))
-                    i += match.end()  # Skip ahead to after the matched alias
-
-        i += 1
-
-    return aliases
-
-def is_subselect(parsed):
-    if not isinstance(parsed[0], Parenthesis):
-        return False
-    for item in parsed.tokens:
-        if item.ttype is DML and item.value.upper() == 'SELECT':
-            return True
-    return False
-
-def find_subquery_aliases(query):
-    # pdb.set_trace()
-    aliases = []
-    parsed = sqlparse.parse(query)
-
-    for statement in parsed:
-        for token in statement.tokens:
-            if is_subselect(token):
-                # Find the next token after the parenthesis which should be the alias
-                next_token_index = statement.token_index(token) + 1
-                if next_token_index < len(statement.tokens):
-                    next_token = statement.tokens[next_token_index]
-                    if isinstance(next_token, Identifier):
-                        aliases.append(next_token.get_name())
-            elif isinstance(token, Identifier):
-                # This is for handling nested subqueries
-                for subtoken in token.tokens:
-                    if is_subselect(subtoken):
-                        alias = token.get_name()
-                        if alias and alias not in aliases:
-                            aliases.append(alias)
-
-    return aliases
 
 def execute_sql_query(conn, query):
     try:
@@ -230,6 +41,7 @@ def execute_sql_query(conn, query):
         return explain_json
     except Exception as e:
         print(f"Failed to execute query: {e}")
+        conn.rollback()
         return None
     finally:
         cur.close()
@@ -247,6 +59,7 @@ def run_explain_query(conn, query, explain="BUFFERS ON, ANALYZE ON, COSTS ON, VE
 
     except Exception as e:
         print(f"Error executing the query: {e}")
+        conn.rollback()
         return None
     finally:
         cur.close()
@@ -297,15 +110,15 @@ def connect_to_db():
         return {"error": "Database connection failed. Try again!"}, 403
 
 
-@app.route('/tables', methods=['GET'])
-def database_tables():
-    token = request.headers.get('Authorization').split(" ")[0]
-    try:
-        tables = execute_sql_query(CONNECTIONS[token], "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname!='pg_catalog' AND schemaname!='information_schema'")
-        tables = [j for i in tables for j in i]
-        return {"Tables": tables}
-    except Exception as e:
-        return {"Error:": str(e)}
+# @app.route('/tables', methods=['GET'])
+# def database_tables():
+#     token = request.headers.get('Authorization').split(" ")[0]
+#     try:
+#         tables = execute_sql_query(CONNECTIONS[token], "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname!='pg_catalog' AND schemaname!='information_schema'")
+#         tables = [j for i in tables for j in i]
+#         return {"Tables": tables}, 200
+#     except Exception as e:
+#         return {"Error:": str(e)}, 400
     
 
 @app.route('/block', methods=['POST'])
@@ -337,7 +150,7 @@ def handle_query():
         
         # If the token is not in connections, that means a connection to the database was never established
         if token not in CONNECTIONS:
-            return jsonify({"error": "Invalid or missing authorization. Check the database connection"})
+            raise Exception("Invalid or missing authorization. Check the database connection")
 
         query = remove_linebreaks_and_extra_spaces(query)
         
@@ -348,7 +161,7 @@ def handle_query():
 
         # If the method to explain query didn't work properly and instead just gave an empty response, this condition is executed
         if results is None:
-            return jsonify({"error": "Failed to execute query."})
+            raise Exception("Failed to execute query")
         # Only to print out the tree in the terminal
         # TODO: Delete this portion later
         else:
@@ -356,75 +169,13 @@ def handle_query():
             for plan in results:
                 print_plan(plan["Plan"])
 
-        return {"results": results, "statistics": stats}
+        return {"results": results, "statistics": stats}, 200
 
     except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Token has expired"})
+        return {"error": "Token has expired"}, 400
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return {"error": str(e)}, 400
+
 
 def backend():    
-    query = """select
-      supp_nation,
-      cust_nation,
-      l_year,
-      sum(volume) as revenue
-    from
-      (
-        select
-          n1.n_name as supp_nation,
-          n2.n_name as cust_nation
-        from
-          supplier,
-        where
-          s_suppkey = l_suppkey
-      ) as shipping,
-      (
-        select
-          n1.n_name as supp_nation,
-          n2.n_name as cust_nation
-        from
-          supplier,
-          lineitem,
-          orders,
-          customer,
-          nation n1,
-          nation n2
-        where
-          s_suppkey = l_suppkey
-          and o_orderkey = l_orderkey
-          and c_custkey = o_custkey
-          and s_nationkey = n1.n_nationkey
-          and c_nationkey = n2.n_nationkey
-          and (
-            (n1.n_name = 'FRANCE' and n2.n_name = 'GERMANY')
-            or (n1.n_name = 'GERMANY' and n2.n_name = 'FRANCE')
-          )
-          and l_shipdate between '1995-01-01' and '1996-12-31'
-          and o_totalprice > 100
-          and c_acctbal > 10
-      ) as dropshipping
-    group by
-      supp_nation,
-      cust_nation,
-      l_year
-    order by
-      supp_nation,
-      cust_nation,
-      l_year;"""
-
-    # Example usage
-#     query = """
-#     SELECT s_suppkey, s_name, SUM(l_extendedprice * (1 - l_discount)) AS total_revenue
-# FROM supplier
-#          JOIN lineitem ON s_suppkey = l_suppkey
-# GROUP BY s_suppkey, s_name
-# ORDER BY total_revenue DESC
-#     """
-    # modified_query = add_ctids_to_query(query)
-    # print(modified_query)
-    # add_ctids_to_query(query)
-    # query = remove_linebreaks_and_extra_spaces(query)
-    # print(query)
-    # print(extract_table_names(query))
     app.run()
